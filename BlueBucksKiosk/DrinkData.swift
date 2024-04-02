4c97e386c8aabf7fa1f72dc49c6466532db6c305
Bread-kn72 <ordinarytom72@gmail.com>
2024-04-02 11:55:02 +0900

    Add: DrinkData 파일생성

---
+++ BlueBucksKiosk/DrinkData.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+struct Drink {
+    var name: (String, String)
+    var image: UIImage
+    var description: String
+    var price: Int
+    var category: Category
+    var size: Size
+}
+
+enum Category {
+    case espresso
+    case frappuccino
+    case teavana
+    case etc
+}
+
+enum Size: Int {
+    case tall = 355
+    case grande = 473
+    case venti = 591
+}
